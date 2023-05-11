cefde6b7d0f1904602381a1af8b1825be3c77b21
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-05-11 20:06:59 -0300

    feat: prisma adapter

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,7 +1,7 @@
 import { Adapter } from "next-auth/adapters";
 import { prisma } from "../prisma";
 
-export default function MyAdapter(): Adapter {
+export default function PrismaAdapter(): Adapter {
   return {
     async createUser(user) {
       return;
@@ -13,39 +13,137 @@ export default function MyAdapter(): Adapter {
           id,
         },
       });
+
       return {
-        id: user?.id,
-        name: user?.name,
-        email: user?.email,
-        username: user?.username,
-        avatar_url: user?.avatar_url,
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        username: user.username,
+        avatar_url: user.avatar_url!,
         emailVerified: null,
       };
     },
+
     async getUserByEmail(email) {
-      return;
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email,
+        },
+      });
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
     },
+
     async getUserByAccount({ providerAccountId, provider }) {
-      return;
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {provider, provider_account_id: providerAccountId}
+        },
+        include: {
+          user: true,
+        }
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
     },
+
     async updateUser(user) {
-      return;
+      const newUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        }
+      })
+
+      return {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email!,
+        username: newUser.username,
+        avatar_url: newUser.avatar_url!,
+        emailVerified: null,
+      };
     },
-    async deleteUser(userId) {
-      return;
-    },
+
     async linkAccount(account) {
-      return;
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        }
+      })
     },
-    async unlinkAccount({ providerAccountId, provider }) {
-      return;
-    },
+
     async createSession({ sessionToken, userId, expires }) {
-      return;
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken,
+        }
+      })
+
+      return {
+        userId,
+        sessionToken,
+        expires,
+      }
     },
+
     async getSessionAndUser(sessionToken) {
-      return;
+      const {user, ...session} = await prisma.session.findUniqueOrThrow({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      return {
+        session: {
+          userId: session.user_id,
+          expires: session.expires,
+          session_token: session.session_token
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,
+          emailVerified: null,
+          avatar_url: user.avatar_url!
+        }
+      }
     },
+
     async updateSession({ sessionToken }) {
       return;
     },
